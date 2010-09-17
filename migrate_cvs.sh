fc26d222ca568aba1abfb869659bdebdbc6c30e1
Magnus Hagander <magnus@hagander.net>
2010-09-17 10:53:07 +0000

    Import Tom's script for cleaning up the repository, and call taht instead.

--- migrate_cvs.sh
+++ migrate_cvs.sh
@@ -4,13 +4,9 @@ set -e
 
 rsync -avzCH --exclude '#cvs.*' --delete anoncvs.postgresql.org::pgsql-cvs /cvsroot
 
-# Remove some broken revisions
-pushd /
-for r in 2.89 2.90 2.91; do rcs -x,v -sdead:$r ./cvsroot/pgsql/src/backend/parser/Attic/gram.c ; done
-for r in 1.3 1.4 1.5 1.6; do rcs -x,v -sdead:$r ./cvsroot/pgsql/src/interfaces/ecpg/preproc/Attic/pgc.c ; done
-for r in 1.7 1.8 1.9 1.10 1.11 1.12; do rcs -x,v -sdead:$r ./cvsroot/pgsql/src/interfaces/ecpg/preproc/Attic/preproc.c ; done
-for r in 1.3 1.4 1.5 1.6 1.7 1.8; do rcs -x,v -sdead:$r ./cvsroot/pgsql/src/interfaces/ecpg/preproc/Attic/y.tab.h ; done
-popd
+# Clean up issues in the existing cvs repository (must always be done *after* the rsync
+# of course, since a new rsync will overwrite it)
+./repository_fixups
 
 
 # Do the conversion
